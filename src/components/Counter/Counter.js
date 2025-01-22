c72db9aba5c430093ea01b4797f9e5932deac242
Lenka Šmitalová <>
2025-01-22 14:59:25 +0100

    Next Server Side Rendering Gotchas solution

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,23 +1,24 @@
 'use client';
 import React from 'react';
 
+import Spinner from '../Spinner';
+
 function Counter() {
-  const [count, setCount] = React.useState(() => {
-    return Number(
-      window.localStorage.getItem('saved-count') || 0
-    );
-  });
+  const [count, setCount] = React.useState(null);
+
+  React.useEffect(() => {
+    const savedCount = window.localStorage.getItem('saved-count');
+
+    setCount(Number(savedCount) || 0);
+  }, []);
 
   React.useEffect(() => {
     window.localStorage.setItem('saved-count', count);
   }, [count]);
 
   return (
-    <button
-      className="count-btn"
-      onClick={() => setCount(count + 1)}
-    >
-      Count: {count}
+    <button className='count-btn' onClick={() => setCount(count + 1)}>
+      Count: {typeof count === 'number' ? count : <Spinner />}
     </button>
   );
 }
