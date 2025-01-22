d7ea77baf6c25c9be1cde1b71550eb8540c3f6d3
Lenka Šmitalová <>
2025-01-22 14:31:35 +0100

    Drum Machine Solution

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,17 +3,20 @@ import React from 'react';
 import Header from '../components/Header';
 
 import './styles.css';
+import SoundProvider from '../components/SoundProvider/SoundProvider';
 
 function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>
-        <Header />
-        {children}
-        <footer>
-          <img src="/ie-badge.gif" width={100} />
-          <span>Thanks for visiting!</span>
-        </footer>
+        <SoundProvider>
+          <Header />
+          {children}
+          <footer>
+            <img src='/ie-badge.gif' width={100} />
+            <span>Thanks for visiting!</span>
+          </footer>
+        </SoundProvider>
       </body>
     </html>
   );
